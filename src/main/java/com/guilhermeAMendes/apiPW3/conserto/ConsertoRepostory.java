13033c37e323ee012a4f719b20bda5fbfce49047
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-07 15:41:38 -0300

    feat: create Repair Repository

---
+++ src/main/java/com/guilhermeAMendes/apiPW3/conserto/ConsertoRepostory.java
@@ -0,0 +1,7 @@
+package com.guilhermeAMendes.apiPW3.conserto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsertoRepostory extends JpaRepository<Conserto, Long> {
+
+}
