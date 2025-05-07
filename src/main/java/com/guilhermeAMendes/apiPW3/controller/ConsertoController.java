ed56f48dc6df4368b57fe93887a4421070a004d0
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-07 16:17:30 -0300

    feat: create repair controller

--- src/main/java/com/guilhermeAMendes/apiPW3/controller/ConsertoController.java
+++ src/main/java/com/guilhermeAMendes/apiPW3/controller/ConsertoController.java
@@ -1,6 +1,10 @@
 package com.guilhermeAMendes.apiPW3.controller;
 
+import com.guilhermeAMendes.apiPW3.conserto.Conserto;
+import com.guilhermeAMendes.apiPW3.conserto.ConsertoRepostory;
 import com.guilhermeAMendes.apiPW3.conserto.DadosCadastroConserto;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("consertos")
 public class ConsertoController {
 
+    @Autowired
+    private ConsertoRepostory repostory;
+
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroConserto dados) {
-        System.out.println(dados);
+        Conserto conserto = new Conserto(dados);
+        repostory.save(conserto);
     }
 }
