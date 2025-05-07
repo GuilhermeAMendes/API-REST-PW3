202c01eae297f235798fdb11805942a64e898e18
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-07 14:41:45 -0300

    feat: create class and Record structure fot mechanic

---
+++ src/main/java/com/guilhermeAMendes/apiPW3/mecanico/DadosCadastroMecanico.java
@@ -0,0 +1,6 @@
+package com.guilhermeAMendes.apiPW3.mecanico;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosCadastroMecanico(@NotBlank String nome, @NotBlank String anosExperiencia) {
+}
---
+++ src/main/java/com/guilhermeAMendes/apiPW3/mecanico/Mecanico.java
@@ -0,0 +1,20 @@
+package com.guilhermeAMendes.apiPW3.mecanico;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mecanico {
+    private String nome;
+    private String anosExperiencia;
+
+    public Mecanico(DadosCadastroMecanico dadosCadastro) {
+        this.nome = dadosCadastro.nome();
+        this.anosExperiencia = dadosCadastro.anosExperiencia();
+    }
+}
