e37e9a81242b244666e4ceb88134a9d82a532f0a
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-07 14:08:17 -0300

    initial commit

---
+++ src/main/java/com/guilhermeAMendes/apiPW3/ApiPw3Application.java
@@ -0,0 +1,13 @@
+package com.guilhermeAMendes.apiPW3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPw3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPw3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermeAMendes/apiPW3/ApiPw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeAMendes.apiPW3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPw3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
