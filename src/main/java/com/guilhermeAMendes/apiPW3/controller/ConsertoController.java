31a812671424e3bf54cc3e58f6f549af7019b864
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-08 02:17:57 -0300

    feat: add pagination in request data method

--- src/main/java/com/guilhermeAMendes/apiPW3/controller/ConsertoController.java
+++ src/main/java/com/guilhermeAMendes/apiPW3/controller/ConsertoController.java
@@ -1,11 +1,12 @@
 package com.guilhermeAMendes.apiPW3.controller;
 
-import com.guilhermeAMendes.apiPW3.conserto.Conserto;
-import com.guilhermeAMendes.apiPW3.conserto.ConsertoRepostory;
-import com.guilhermeAMendes.apiPW3.conserto.DadosCadastroConserto;
-import com.guilhermeAMendes.apiPW3.conserto.DadosListagemConserto;
+import com.guilhermeAMendes.apiPW3.conserto.*;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,8 +26,13 @@ public class ConsertoController {
     }
 
     @GetMapping
-    public List<Conserto> listarTodosOsConsertos() {
-        return repostory.findAll();
+    public Page<DadosDetalhadosConserto> listarTodos(@PageableDefault(
+            size = 10,
+            page = 0,
+            sort = {"mecanico.nome"},
+            direction = Sort.Direction.DESC
+    ) Pageable pageable) {
+        return repostory.findAll(pageable).map(DadosDetalhadosConserto::new);
     }
 
     @GetMapping("algunsdados")
