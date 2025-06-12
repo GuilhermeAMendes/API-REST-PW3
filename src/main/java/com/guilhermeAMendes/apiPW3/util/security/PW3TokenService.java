4a16ff2c485527b3625e9f143059e9a8ce86db7a
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-06-12 12:25:38 -0300

    feat: configure security filters and header validation of token

--- src/main/java/com/guilhermeAMendes/apiPW3/util/security/PW3TokenService.java
+++ src/main/java/com/guilhermeAMendes/apiPW3/util/security/PW3TokenService.java
@@ -1,10 +1,11 @@
 package com.guilhermeAMendes.apiPW3.util.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.guilhermeAMendes.apiPW3.usuario.Usuario;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -28,11 +29,28 @@ public class PW3TokenService {
                     .withExpiresAt(dataExpiracao())
                     .sign(algoritmo);
             return token;
-        } catch (JWTCreationException exception){
+        } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token jwt", exception);
         }
     }
+
+    public String getSubject(String tokenJWT) {
+
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            JWTVerifier jwtv = JWT.require(algoritmo)
+                    .withIssuer("DISCIPLINA PW3")
+                    .build();
+            return jwtv.verify(tokenJWT).getSubject();
+
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
+
+
 }
